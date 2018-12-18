54d09e06968a04bc72b074aa8db77908707c5b77
fuadmiazi <fuad.hasan.1997@gmail.com>
2018-12-19 05:34:07 +0600

    Insertion algorithm

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -1,4 +1,7 @@
 #include "InsertionSort.h"
+#include <iostream>
+
+using namespace std;
 
 InsertionSort::InsertionSort()
 {
@@ -15,16 +18,22 @@ void InsertionSort::InsertionSorter(int arr[], int arr_size)
     if(arr_size > 1)
     {
         int size = arr_size;
-        for(int i=1; i<size; ++i)
+        for(int index=1; index<size; ++index)
         {
-            int j = i-1;
-            int key = arr[i];
+            int j = index-1;
+            int key = arr[index];
             while(j>=0 && arr[j] > key)
             {
                 arr[j+1] = arr[j];
                 --j;
             }
             arr[j+1] = key;
+            cout<<"Steps of Insertion Sort"<<endl;
+            for(int index =0; index<arr_size; index++)
+            {
+                cout<<arr[index]<<" ";
+            }
+            cout<<""<<endl;
         }
     }
 }
